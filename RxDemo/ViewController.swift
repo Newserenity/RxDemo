3dd04e06926fc4848c9ff6f41d1ac04a6c29c0c5
Jayden Jang <newcephei@icloud.com>
2023-05-22 23:11:42 +0900

    storyboard

--- RxDemo/ViewController.swift
+++ RxDemo/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FirstLabel: UILabel!
+    @IBOutlet weak var firstCount: UILabel!
+    @IBOutlet weak var firstInput: UITextField!
+    
+    @IBOutlet weak var secoundLabel: UILabel!
+    @IBOutlet weak var secoundCount: UILabel!
+    @IBOutlet weak var secoundInput: UITextField!
+    
+    @IBOutlet weak var warningLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

33567f6b8d3616d004edd7b51864aec6710ae35e
Jayden Jang <newcephei@icloud.com>
2023-05-24 12:55:07 +0900

    final

--- RxDemo/ViewController.swift
+++ RxDemo/ViewController.swift
@@ -29,43 +29,65 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        firstInput.rx.text
+        createObservables()        
+    }
+    
+    func createObservables() {
+        let firstHasValue : Observable<Bool> = firstInput.rx.text.orEmpty
+            .map{ $0.count > 0 }
+            .share(replay: 1)
+        
+        let secondHasValue : Observable<Bool> = secoundInput.rx.text.orEmpty
+            .map{ $0.count > 0 }
+            .share(replay: 1)
+        
+        let firstInputText: Observable<String> = firstInput.rx.text
             .orEmpty
             .map { $0.isEmpty ? "입력하세요" : $0 }
-            .bind(to: firstLabel.rx.text)
+        
+        let secoundInputText: Observable<String> = secoundInput.rx.text
+            .orEmpty
+            .map { $0.isEmpty ? "입력하세요" : $0 }
+        
+        Observable
+            .combineLatest(firstHasValue, secondHasValue)
+            .map{ $0 == true && $1 == true } // 둘다 입력이 있는지 여부
+            .map{ $0 ? "OK" : "첫번째, 두번째를 입력하세요" }
+            .debug("⭐️ combineLatest")
+            .bind(to: warningLabel.rx.text)
             .disposed(by: disposeBag)
         
-        firstInput.rx.text
-            .orEmpty
-            .map { $0.count.description }
+        firstInputText
+            .subscribe(onNext: { [weak self] text in
+                self?.firstLabel.text = text
+            })
+            .disposed(by: disposeBag)
+        
+        secoundInputText
+            .subscribe(onNext: { [weak self] text in
+                self?.secoundLabel.text = text
+            })
+            .disposed(by: disposeBag)
+        
+        firstInput.rx.text.orEmpty
+            .map { "\($0.count)" }
             .bind(to: firstCount.rx.text)
             .disposed(by: disposeBag)
-
-        secoundInput.rx.text
-            .orEmpty
-            .map { $0.isEmpty ? "입력하세요" : $0 }
-            .bind(to: secoundLabel.rx.text)
-            .disposed(by: disposeBag)
         
-        secoundInput.rx.text
-            .orEmpty
-            .map { $0.count.description }
+        secoundInput.rx.text.orEmpty
+            .map { "\($0.count)" }
             .bind(to: secoundCount.rx.text)
-            .disposed(by: disposeBag)
-        
-        Observable.combineLatest(
-            firstInput.rx.text.orEmpty,
-            secoundInput.rx.text.orEmpty)
-            .map { firstInputText, secoundInputText in
-                return !firstInputText.isEmpty || !secoundInputText.isEmpty ? "OK" : "첫번째, 두번째를 입력하세요"
-            }
-            .bind(to: warningLabel.rx.text)
             .disposed(by: disposeBag)
         
         reset.rx.tap
             .subscribe(onNext: { [weak self] in
                 self?.firstInput.text = ""
                 self?.secoundInput.text = ""
+                self?.firstLabel.text = "입력하세요"
+                self?.secoundLabel.text = "입력하세요"
+                self?.firstCount.text = "0"
+                self?.secoundCount.text = "0"
+                self?.warningLabel.text = "첫번째, 두번째를 입력하세요"
             })
             .disposed(by: disposeBag)
     }
